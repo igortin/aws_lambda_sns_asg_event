15986e5332d45d962dd6b3b0d5a429082b3f1a95
Igor Tin <igor.tin@camelotls.com>
2020-07-17 23:10:13 +0600

    Append trigger on event

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# aws_lambda_sns_asg_event
-ASG change state -> ClowdWatch ALARM -> SNS -> LAMBDA -> Action.
+# lambda_sns_event
+ClowdWatch ALARM -> SNS -> LAMBDA -> Action.
 - ClowdWatch ALARM configured based on CloudWatch Metrics and send event to SNS 
 - SNS subscribed by Lambda func
 - Labmda unmarshal string to instance &Message{}
 
 After you can use it as you want and send any API request to remediate ALARM situation.
+# aws_lambda_sns_asg_event
--- main.go
+++ main.go
@@ -2,38 +2,101 @@ package main
 
 import (
 	"context"
-	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
-	"reflect"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/autoscaling"
 )
 
 func handler(ctx context.Context, event events.SNSEvent) {
+	svc := autoscaling.New(session.New())
 	for _, record := range event.Records {
-		snsRecord := record.SNS
-
-		fmt.Printf("[%s %s]\n", record.EventSource, snsRecord.Timestamp)
-
-		msg := &Message{}
-		err := json.Unmarshal([]byte(record.SNS.Message), msg)
+		fmt.Println("EventSource:", record.EventSource)
+		fmt.Println("EventTimestamp:", record.SNS.Timestamp)
+		// parameter pointer to record instance
+		msg, err := parseEvent(&record)
 		if err != nil {
-			log.Println("Check structure Message:", err)
+			log.Println("checkEventParameters:", err)
 		}
-		fmt.Println("Success:", reflect.TypeOf(msg))
-		fmt.Println("AlarmName:", msg.AlarmName)
-		fmt.Println("Trigger MetricName:", msg.Trigger.MetricName)
 
-		for _, item := range msg.Trigger.Dimensions {
-			fmt.Println("Success:", reflect.TypeOf(item))
-			fmt.Println("Success:", item.ResourceType)
-			fmt.Println("Success:", item.ResourceName)
+		isTrue, err := Remediation(svc, msg)
+		if err != nil {
+			log.Println(err)
 		}
+		fmt.Println("Remediation:", isTrue)
 	}
 }
 
 func main() {
 	lambda.Start(handler)
+}
+
+func parseEvent(record *events.SNSEventRecord) (*Message, error) {
+	// Init instance
+	msg := NewMessage()
+	// Create byte array
+	b := []byte(record.SNS.Message)
+
+	err := msg.Unmarshal(b)
+	if err != nil {
+		return nil, err
+	}
+	return msg, nil
+}
+
+// Remediation func to align problem
+func Remediation(svc *autoscaling.AutoScaling, msg *Message) (bool, error) {
+	var isTrue bool
+	if msg.Trigger.MetricName == "CPUUtilization" {
+		for _, resource := range msg.Trigger.Dimensions {
+			if resource.ResourceType == "AutoScalingGroupName" {
+				isScale, err := scaleAsg(svc, resource.ResourceName, int64(msg.Trigger.Threshold+1))
+				if err != nil {
+					return false, err
+				}
+				isTrue = isScale
+			} else {
+				log.Println("Unknow resource.ResourceType")
+			}
+		}
+	} else {
+		fmt.Println("Unknown msg.Trigger.MetricName")
+	}
+	return isTrue, nil
+}
+
+func showEventReason(msg *Message) {
+	fmt.Println("AlarmName:", msg.AlarmName)
+	fmt.Println("MetricName:", msg.Trigger.MetricName)
+	fmt.Printf("Threshold %v value was exceed:", msg.Trigger.Threshold)
+}
+
+func scaleAsg(svc *autoscaling.AutoScaling, asgName string, desireValue int64) (bool, error) {
+	input := &autoscaling.SetDesiredCapacityInput{
+		AutoScalingGroupName: aws.String(asgName),
+		DesiredCapacity:      aws.Int64(desireValue),
+		HonorCooldown:        aws.Bool(true),
+	}
+	_, err := svc.SetDesiredCapacity(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case autoscaling.ErrCodeScalingActivityInProgressFault:
+				return false, errors.New(fmt.Sprintln(autoscaling.ErrCodeScalingActivityInProgressFault, aerr.Error()))
+			case autoscaling.ErrCodeResourceContentionFault:
+				return false, errors.New(fmt.Sprintln(autoscaling.ErrCodeResourceContentionFault, aerr.Error()))
+			default:
+				return false, errors.New(aerr.Error())
+			}
+		} else {
+			return false, errors.New(aerr.Error())
+		}
+	}
+	return true, nil
 }
